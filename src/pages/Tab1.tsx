81ba6b5476b7050eecdb4b58722934d4ae940ade
Tobias Lorsbach <hallo@tobeworks.de>
2021-03-10 10:52:15 +0100

    not wokring with props

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -1,11 +1,6 @@
-<<<<<<< HEAD
 import React from 'react';
 import {IonPage, IonMenu, IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonRouterOutlet, IonButton, IonMenuButton } from '@ionic/react';
 
-=======
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
->>>>>>> test2
-import ExploreContainer from '../components/ExploreContainer';
 import './Tab1.css';
 
 const Tab1: React.FC = () => {
@@ -19,7 +14,6 @@ const Tab1: React.FC = () => {
           <IonTitle>Tab 1</IonTitle>
         </IonToolbar>
       </IonHeader>
-<<<<<<< HEAD
 
       <IonMenu side="start" menuId="first" contentId="foo" id="first">
       <IonHeader>
@@ -41,9 +35,6 @@ const Tab1: React.FC = () => {
       <IonContent fullscreen id="foo">
 
       
-=======
-      <IonContent fullscreen id="foo">
->>>>>>> test2
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 1</IonTitle>
